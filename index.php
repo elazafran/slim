47f10d8857e0c406f4d039e24aadd4c27a1f3467
elazafran <javiealiaga@gmail.com>
2018-08-17 10:09:59 +0200

    Version 2 condiciones opcionales

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ require 'vendor/autoload.php';
  * your Slim application now by passing an associative array
  * of setting names and values into the application constructor.
  */
-$app = new \Slim\App;
+$app = new \Slim\Slim();
 /**
  * Step 3: Define the Slim application routes
  *
@@ -34,36 +34,22 @@ $app = new \Slim\App;
 
 /******************** Ruteador ******************/
 
-$app->get('/{name}', function ($request, $response, $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Welcome , $name");
-    return $response;
 
-    // http://localhost/slim/javier
-});
-
-$app->get('/hello/{id}/{surname}', function ($request, $response, $args) {
-    $id = $args['id'];
-    $surname = $args['surname'];
-    $response->getBody()->write("<h1>Hello, $id - $surname</h1>");
-    return $response;
-
-    // http://localhost/slim/hello/javier/aliaga
-});
-
-
-$app->get('/p/{id}/{slug}', function ($request, $response, $args) {
-    //Show book identified by $id
-    $id = $args['id'];
-    $slug = $args['slug'];
-    $response->getBody()->write(" Post $id $slug ");
-    return $response;
+$app->map('/p/:id(/:slug)', function ($id, $slug="por defecto") use ($app) {
+    if ($app->request->isGet()){
+        echo " Get ";
+        echo " $id $slug ";
+        // http://localhost/slim/p/123/asd
+        // http://localhost/slim/p/123/
+    }else {
+        echo " post ";
+    }
 
     // http://localhost/slim/p/123/el%20se%C3%B1or%20de%20los%20a%C3%B1illos
 
-});
+})->via(['GET','POST'])->conditions(['id' => '\d+'])->name('blog.index');
 
-// curl -X POST http://localhost:8888/slim
+// curl -X POST http://localhost/slimp/123/el%20se%C3%B1or%20de%20los%20a%C3%B1illos
 
 /**
  * Step 4: Run the Slim application
