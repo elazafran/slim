46e14913495cb680ea6dfbe03c6d751bdd855787
elazafran <javiealiaga@gmail.com>
2018-08-17 10:49:09 +0200

    templates y paso de variables, también codigo que queremos http que se quiere lanzar

--- index.php
+++ index.php
@@ -9,44 +9,19 @@
 require 'vendor/autoload.php';
 //registramos el autoloader
 
-/**
- * Step 2: Instantiate a Slim application
- *
- * This example instantiates a Slim application using
- * its default settings. However, you will usually configure
- * your Slim application now by passing an associative array
- * of setting names and values into the application constructor.
- */
 $app = new \Slim\Slim();
 
-/******************** Ruteador 4 condiciones ******************/
-function mw1()
-{
-    echo "hola ";
-}
+$app->get('/',function () use ($app){
 
-function mw2()
-{
-    $app =\Slim\Slim::getInstance();//nos devuelve la instacia de ap que ya está creada
-    $app->redirect('/slim/inicio');
-}
+    // opcion 1 para pasar parametros
+   // $app -> render('index.php',['name'=>'javier aliaga']);
 
-$app->get('/inicio',function (){
-    echo "<h1>inicio</h1>";
+    // opcion 2 para pasar parametros
+    $name = "javier";
+    $surname = "aliaga";
+    $app -> render('index.php',compact('name','surname'), 404);
+
     // http://localhost/slim/inicio
 });
-
-$app->get('/hola/:firstName(/:lastName)','mw1','mw2', function($firstName,$lastName="mohamed") use ($app){
-
-    if($app->request->isGet()){
-        echo "Hola $firstName $lastName ";
-    }
-    // http://localhost/slim/hola/javier/aliaga
-
-})->via(['GET','POST'])->conditions(array('lastName' => '[a-zA-Z]{3,}'))->name('blog.index');
-
-
-
-
 
 $app->run();
