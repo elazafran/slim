705a4a5f55c352b93f91cb108484ae8972d70490
elazafran <javiealiaga@gmail.com>
2018-08-17 11:26:21 +0200

    Cookies encryptadas

--- index.php
+++ index.php
@@ -6,14 +6,28 @@
  * If you are not using Composer, you need to load Slim Framework with your own
  * PSR-4 autoloader.
  */
-require 'vendor/autoload.php';
-//registramos el autoloader
 
-$app = new \Slim\Slim();
+require 'vendor/autoload.php';
+
+// cookies encriptadas
+$app = new \Slim\Slim([
+    'cookies.encrypt' => true,
+    'cookies.secret_key' => 'encriptacion de la cookie'
+]);
 
 $app->map('/',function () use ($app){
 
    if($app->request->isGet()){
+        // creamos la cookie
+       $app->setCookie('user_id','1234','5 minutes');
+       // borramos la cookie
+       //$app->deleteCookie('user_id');
+       // accedemos a la cookie de 2 formas
+       // 1ª manera
+       echo $app->request->cookies('user_id');
+       echo "<br>";
+       // 2ª forma
+       echo $app->getCookie('user_id');
        $app -> render('form.php');
    }elseif ($app->request->isPost()){
        $name = $app->request->post('name');
